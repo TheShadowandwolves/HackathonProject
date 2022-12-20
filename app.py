658b482715dfe9982ef874c471daab55ab17cfdb
Leonard Blam <blam@g.jct.ac.il>
2022-12-20 16:45:58 +0200

    d

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from HackathonProject import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
--- routes.py
+++ routes.py
@@ -10,15 +10,3 @@ import random
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/form')
-def paper():
-    return render_template('form.html')
