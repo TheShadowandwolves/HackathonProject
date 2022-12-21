1d1051bf69247b4a9a1b38f5e36d86627f11d38e
Leonard Blam <blam@g.jct.ac.il>
2022-12-21 10:45:47 +0200

    f

--- routes.py
+++ routes.py
@@ -1,14 +1,19 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, jsonify
 from HackathonProject import app
 import random
 
 
-data = "home"
+
 ### app routes ###
 
 @app.route('/')
 @app.route('/home')
 def home():
-
+    data ={ "typeName": "test"}
     print("dic")
-    return render_template('home.html', dat = data)
+    return render_template('home.html', data = data)
+
+@app.route('/data')
+def get_data():
+    data = {'key': 'new value'}
+    return jsonify(data)
