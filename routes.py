f60f7d9267fc7028854ebc1be8e3d60f642bde68
Leonard Blam <blam@g.jct.ac.il>
2022-12-21 13:03:54 +0200

    page

--- routes.py
+++ routes.py
@@ -1,7 +1,17 @@
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from HackathonProject import app
 import random
+import requests
+import json
 
+def get_location():
+    send_url = "http://api.ipstack.com/check?access_key=YOUR_ACCESS_KEY"
+    geo_req = requests.get(send_url)
+    geo_json = json.loads(geo_req.text)
+    latitude = geo_json['latitude']
+    longitude = geo_json['longitude']
+    city = geo_json['city']
+    return city
 
 
 ### app routes ###
@@ -9,7 +19,13 @@ import random
 @app.route('/')
 @app.route('/home')
 def home():
-    data ={ "typeName": "test"}
+    data ={ "typeName": "test", 
+            "date": "date",
+            "time": "time",
+            "location": "location",
+            "currentlocation": get_location()
+
+            }
     print("dic")
     return render_template('home.html', data = data)
 
