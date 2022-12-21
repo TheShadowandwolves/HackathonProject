578613cd94b8edc9b9719fce2d9e907162a5bad7
Leonard Blam <blam@g.jct.ac.il>
2022-12-21 13:11:31 +0200

    d

--- routes.py
+++ routes.py
@@ -3,14 +3,8 @@ from HackathonProject import app
 import random
 import requests
 import json
-
 def get_location():
-    send_url = "http://api.ipstack.com/check?access_key=YOUR_ACCESS_KEY"
-    geo_req = requests.get(send_url)
-    geo_json = json.loads(geo_req.text)
-    latitude = geo_json['latitude']
-    longitude = geo_json['longitude']
-    city = geo_json['city']
+    city = "New York"
     return city
 
 
